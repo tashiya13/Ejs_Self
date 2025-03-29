99267642c69889f2bd32e71e115b694ec3154b1f
anshu-technosters-net <anshu@technosters.net>
2025-03-29 16:47:02 +0530

    12.59 in youtube

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
-app.set('view options', {delimiter: '?'});
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Set view engine to ejs
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views'); // Optional: Specify the folder where the views are stored
 
 app.get('/', (req, res) => {
-  let siteName = 'adidas'
-  let searchText = 'Search Now'
-  res.sendFile("app/app.html",{root:__dirname})
-})
+  let siteName = 'adidas';
+  let searchText = 'Search Now';
+  
+  // Render the EJS file 'app.ejs' located in the 'views' directory
+  res.render("app", { siteName: siteName, searchText: searchText });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
+
+
+
+
